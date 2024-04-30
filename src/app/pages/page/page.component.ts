7d1cb119a30ea0b50d475166cacd2aa4300e37e6
Luuk <me@luuk.network>
2024-04-30 13:53:13 +0200

    Fixed some routes

--- src/app/pages/page/page.component.ts
+++ src/app/pages/page/page.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page',
